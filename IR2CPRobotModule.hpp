46a08aaf08fd03ee0cf14c6cbe3ebdb337d59107
Filip <fgarmaz@outlook.com>
2024-02-21 11:24:03 +0100

    Initial version of robot modules interface.

---
+++ IR2CPRobotModule.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+
+#include "R2CPCommand.hpp"
+
+
+
+namespace R2CP {
+
+class IR2CPRobotModule {
+public:
+    virtual std::vector<R2CPCommand> evaluatereactive(const R2CPCommand&) = 0;
+    virtual std::vector<R2CPCommand> evaluateactive() = 0;
+};
+
+} // End of namespace: R2CP
